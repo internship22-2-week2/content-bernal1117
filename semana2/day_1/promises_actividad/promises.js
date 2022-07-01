6645467d0ce731fa933b9ce8388f3d111e7f3886
bernal1117 <80118669+bernal1117@users.noreply.github.com>
2022-07-01 00:00:29 +0000

    adsad

--- semana2/day_1/promises_actividad/promises.js
+++ semana2/day_1/promises_actividad/promises.js
@@ -1,116 +1,51 @@
 //ACTIVIDDAD PROMISES
- const https = require('https');
+import https from 'https';
+
+function requestLink(endpoint){
+    return new Promise((resolve, reject) =>{
+        https.get(endpoint, (result) =>{
+            let data = ""
+            result.on("data", (chunk) =>{
+                data += chunk
+            })
+            result.on("end", ()=>{
+                resolve(JSON.parse(data));
+            });
+
+            }).on("error", (error)=>{
+                reject(error)
+            console.log('statusCode:', result.statusCode);
+    
+        })
+    })
+}
+
+const promise1 = requestLink("https://flagcdn.com/es/codes.json")
+const promise2 = requestLink("https://jsonplaceholder.typicode.com/posts")
+const promise3 = requestLink("https://jsonplaceholder.typicode.com/posts/2")
+
+async function request(link){
+    const data = await requestLink(link)
+    console.log(data)
+}
+
+ //promise.all
+Promise.all([promise1, promise2, promise3]).then(resultado=>{
+    console.log(resultado);
+}).catch(error=>{
+    console.log(error)
+}) 
  
- const promesa1 = new Promise((resolve, reject) => { 
-   https.get('https://flagcdn.com/es/codes.json', (resp) =>{
-        let data = '';
-        resp.on('data', (chunk) =>{
-            data += chunk;
-        });
-        resp.on('end', ()=>{
-            resolve(JSON.parse(data));
-        });
-    }).on('error',(error)=>{
-        reject(error)
-    });
-});
-const promesa2 = new Promise((resolve, reject) => { 
-    https.get('https://jsonplaceholder.typicode.com/posts', (resp) =>{
-        let data = '';
-        resp.on('data', (chunk) =>{
-            data += chunk;
-        });
-        resp.on('end', ()=>{
-            resolve(JSON.parse(data));
-        });
-    }).on('error',(error)=>{
-        reject(error)
-    });
-});
-const promesa3 = new Promise((resolve, reject) => { 
-    https.get('https://jsonplaceholder.typicode.com/posts/2', (resp) =>{
-         let data = '';
-         resp.on('data', (chunk) =>{
-             data += chunk;
-         });
-         resp.on('end', ()=>{
-             resolve(JSON.parse(data));
-         });
-     }).on('error',(error)=>{
-         reject(error)
-     });
- });
 
-  let promesas = [promesa1,promesa2,promesa3];
-  //promise.all
-  Promise.all(promesas).then(resultados=>{
-    console.log(resultados)
-  }, error =>{
+//promise.race
+ Promise.race([promise1, promise2, promise3]).then(resultado=>{
+    console.log(resultado);
+}).catch(error=>{
     console.log(error)
-  })
+})  
 
- //promise.race
-  Promise.race(promesas).then(fvalor=>{ //promise.race devuelve la promesa que se ejecute primero
-    console.table(fvalor)
-  },error =>{
-    console.log(error)
-  })
-  
-
-//FORMA ASINCRONA
-function requestJsonPlaceholder(){
-    return new Promise((resolve,reject)=>{
-        https.get('https://flagcdn.com/es/codes.json', (resp) =>{
-            let data = '';
-            resp.on('data', (chunk) =>{
-                data += chunk;
-            });
-            resp.on('end', ()=>{
-                resolve(JSON.parse(data));
-            });
-        }).on('error',(error)=>{
-            reject(error)
-        });
-    });
-}
-function requestJsonPost(){
-    return new Promise((resolve,reject)=>{
-        https.get('https://jsonplaceholder.typicode.com/posts', (resp) =>{
-            let data = '';
-            resp.on('data', (chunk) =>{
-                data += chunk;
-            });
-            resp.on('end', ()=>{
-                resolve(JSON.parse(data));
-            });
-        }).on('error',(error)=>{
-            reject(error)
-        });
-    });
-}
-function requestJsonOnePost(){
-    return new Promise((resolve,reject)=>{
-        https.get('https://jsonplaceholder.typicode.com/posts/2', (resp) =>{
-            let data = '';
-            resp.on('data', (chunk) =>{
-                data += chunk;
-            });
-            resp.on('end', ()=>{
-                resolve(JSON.parse(data));
-            });
-        }).on('error',(error)=>{
-            reject(error)
-        });
-    });
-}
-
-async function realizarPeticion(){
-    let mostrar1 = await requestJsonPlaceholder()
-    let mostrar2 = await requestJsonPost()
-    let mostrar3 = await requestJsonOnePost()
-    console.table(mostrar1)
-    console.table(mostrar2)
-    console.table(mostrar3)
-}
-
-realizarPeticion();
+//asincronica
+request("https://flagcdn.com/es/codes.json")
+request("https://jsonplaceholder.typicode.com/posts")
+request("https://jsonplaceholder.typicode.com/posts/2")
+ 
